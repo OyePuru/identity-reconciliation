f2b0bec18a668e4081d3c8951568dc95d31106e9
Purushottam <mandalpurushottam66@gmail.com>
2024-06-29 04:59:13 +0530

    master: add env for production

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,15 +1,19 @@
 import { DataSourceOptions } from "typeorm";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const ormconfig: DataSourceOptions = {
   type: "mysql",
-  host: "localhost",
-  port: 33066,
-  username: "root",
-  password: "root",
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER_NAME,
+  password: process.env.MYSQL_USER_PASSWORD,
   database: "identity-reconciliation",
   entities: ["src/**/entities/*.entity{.ts,.js}"],
   migrations: ["src/database/migrations/*.ts"],
   synchronize: false
 };
+
+console.log(ormconfig)
 
 export default ormconfig;
